1e282d37c513b5a0b792c978470e22911fb07d5c
amatt-zacali <119893298+amatt-zacali@users.noreply.github.com>
2022-12-05 21:45:30 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,39 @@
+function FeuR () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    strip.shift(1)
+    strip.show()
+    basic.pause(10000)
+    strip.shift(0)
+    basic.clearScreen()
+}
+function FeuV () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    strip.shift(1)
+    strip.show()
+    basic.pause(10000)
+    strip.shift(0)
+    basic.clearScreen()
+}
+function FeuO () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    strip.shift(1)
+    strip.show()
+    basic.pause(2000)
+    strip.shift(0)
+    basic.clearScreen()
+}
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 1, NeoPixelMode.RGB)
+let range = strip.range(0, 3)
+strip.setBrightness(32)
+strip.shift(0)
 basic.forever(function () {
-	
+    while (true) {
+        FeuV()
+        basic.pause(10000)
+        FeuO()
+        basic.pause(2000)
+        FeuR()
+        basic.pause(10000)
+    }
 })
